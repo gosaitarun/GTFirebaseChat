e8eba8cd7cc24b8960de8d2fd4da397e957b55a0
Sanjay Shah <sanjay.shah@sanjay-shah.local>
2017-07-11 15:24:45 +0530

    initial commit

---
+++ GTFirebaseChat/GTFirebaseChat/ViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GTFirebaseChat
+//
+//  Created by Sanjay Shah on 11/07/17.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
